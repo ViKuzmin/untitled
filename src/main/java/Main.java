58a21f862f231a6ceb076d3a6df773b33859d63b
kuzmin <kuzmin>
2022-07-16 17:51:50 +0300

    Add v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
 
+    private int age = 10;
+
+    private boolean getAccess() {
+        return age >= 18;
+    }
+
     public static void main(String[] args) {
 
         System.out.printf("ASDw");
