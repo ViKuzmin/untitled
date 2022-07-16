47f33e63512e41e024592e5581c21d9c1fdc0e45
kuzmin <kuzmin>
2022-07-16 17:42:29 +0300

    Add v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,33 @@
 public class Main {
 
+    private String name = "Man";
+
     public static void main(String[] args) {
 
-        System.out.printf("ASDw");
+
+        Main main = new Main();
+        main.setName("Goga");
+
+        main.sayHello();
+        main.sayBy();
+
+    }
+
+    public void sayHello() {
+
+        System.out.println("Hello " + this.name + "!");
+    }
+
+    public void sayBy() {
+
+        System.out.println("Bye " + this.name + "!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
